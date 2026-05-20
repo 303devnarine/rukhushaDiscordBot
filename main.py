ffef39dbc6e6d369a6550aab0e5ba45a79fabc6a
Dev Narine <129533145+303devnarine@users.noreply.github.com>
2026-05-20 14:13:54 -0400

    Refactor tarot import and fix card response format

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 from inspirational_quotes import quote
 from threading import Thread
 from flask import Flask
-from tarot import Card
+import tarot
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
@@ -70,7 +70,7 @@ async def inspirational_quote(interaction: discord.Interaction):
 @bot.tree.command(name="tarot", description="Sends you a random tarot card, with its meaning.")
 async def tarot(interaction: discord.Interaction):
     card = tarot.drawCard()
-    await interaction.response.send_message(f"{card.id} **{card.name}** ({card.suit})\n{card.meaning}")
+    await interaction.response.send_message(f"{card["id"]} **{card["name"]}** ({card["suit"]})\n{card["meaning"]")
 
 if __name__ == "__main__":
    # Start Flask server in a separate thread
