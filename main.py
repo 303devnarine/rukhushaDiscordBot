1676499e9a97ac302b560cb452af69de23a6035c
Dev Narine <129533145+303devnarine@users.noreply.github.com>
2026-09-14 12:55:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -62,6 +62,8 @@ async def inspirational_quote(interaction: discord.Interaction):
 @bot.tree.command(name="tarot", description="Sends you a random tarot card, with its meaning.")
 async def tarot(interaction: discord.Interaction):
    await interaction.response.send_message("/tarot feature is still in development!")
+   sleep(5)
+   await interaction.response.send_message(tarot.drawCard())
 
 if __name__ == "__main__":
    # Start Flask server in a separate thread
