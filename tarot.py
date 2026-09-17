5b3b193b9d494618988c4a98ca46fe8ab94fdd81
Dev Narine <129533145+303devnarine@users.noreply.github.com>
2026-09-17 11:33:52 -0400

    Update tarot.py

--- tarot.py
+++ tarot.py
@@ -99,5 +99,7 @@ class TarotDeck:
         Card(77, "King of Pentacles",  Suit.PENTACLES, "Wealth, business, leadership, security, discipline",  "Financially inept, obsessed with wealth, stubborn"),
     ]
 
-	def drawCard():
-	    return "card"
+	def drawCard(self):
+	    return cards
+
+
